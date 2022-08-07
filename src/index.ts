22091a78796b621591d6fdd09a9735f6b7f82916
tycreated <110621523+tycreated@users.noreply.github.com>
2022-08-06 22:10:37 -0500

    New drag

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,29 @@
-import gsap from 'gsap'
+import { gsap } from "gsap";
+import { Flip } from "gsap/Flip";
+import { Draggable } from "gsap/Draggable";
 
-gsap.to('.linkbase', { xPercent: 100, duration: 3, ease: 'power2.out' })
+gsap.registerPlugin(Flip, Draggable);
 
+const bars = gsap.utils.toArray('.box'),
+      container1 = document.querySelector('.container1'),
+      container2 = document.querySelector('.container2');
+
+document.querySelector('.linkbase').addEventListener('click', () => {
+  // Get the initial state
+  const state = Flip.getState(bars);
+  
+
+  //Switch the parent
+  let newContainer = bars[0].parentElement === container1 ? container2 : container1;
+  bars.forEach(bar => newContainer.appendChild(bar));
+  
+  // Do the animation!
+  Flip.from(state, {
+    // Optional properties related to HOW it's transitioned
+    duration: 0.8,
+    stagger: 0.05,
+    absolute: true,
+    scale: true,
+    ease: "Power1.out"
+  });
+});
